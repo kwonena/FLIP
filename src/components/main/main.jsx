1fbf5daff46f9911aa1331a446bca3a77c590b1c
KwonEna <eeena214@gmail.com>
2022-01-05 18:53:16 +0900

    Style : main component styling

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -12,13 +12,20 @@ const Main = (props) => {
         <div className={styles.quiz}>
           <span className={styles.text}>안녕하세요! 마스외전님</span>
           <div className={styles.quizBox}>
-            <span>
+            <span className={styles.quizText}>
               이제까지 정리한
               <br />
               지식을 검증해보고 싶다면?
             </span>
-            <button className={styles.button}>퀴즈 풀러 가기</button>
+            <button className={styles.quizBtn}>퀴즈 풀러 가기</button>
           </div>
+        </div>
+        <div className={styles.study}>
+          <div className={styles.studyBox}>
+            <span className={styles.studyText}>학습 중</span>
+            <button className={styles.studyBtn}>문제집 추가</button>
+          </div>
+          <div className={styles.cards}>카드 컴포넌트 들어갈 자리</div>
         </div>
       </section>
     </>
