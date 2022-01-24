6e7f67312040d60a30bced249ca0b1357554b7aa
KwonEna <eeena214@gmail.com>
2022-01-25 02:20:13 +0900

    Style : book component 수정/삭제 UI 변경

--- src/components/mainBook/book.jsx
+++ src/components/mainBook/book.jsx
@@ -2,31 +2,38 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./book.module.css";
 
-const Book = ({ book, loadCards }) => {
+const Book = ({ book, loadCards, onDelete }) => {
   const navigate = useNavigate();
+  const deleteCard = () => {
+    onDelete(book);
+  };
 
   return (
-    <li
-      className={styles.container}
-      onClick={() => {
-        console.log("book");
-        navigate("/addBook", {
-          state: {
-            id: book.id,
-            title: book.title,
-            cards: book.cards,
-          },
-        });
-      }}
-    >
+    <li className={styles.container}>
       <div className={styles.item}>
         <section>
           <div className={styles.bookTitle}>{book.title}</div>
           <div className={styles.bookCount}>{book.cards.length}개의 카드</div>
         </section>
         <footer className={styles.bookFooter}>
-          <i>수정 </i>
-          <i className={styles.bookDelete}>삭제</i>
+          <button
+            className={styles.bookEdit}
+            onClick={() => {
+              console.log("book");
+              navigate("/addBook", {
+                state: {
+                  id: book.id,
+                  title: book.title,
+                  cards: book.cards,
+                },
+              });
+            }}
+          >
+            <i class="fas fa-edit fa-2x"></i>
+          </button>
+          <button className={styles.bookDelete} onClick={deleteCard}>
+            <i class="fas fa-trash-alt fa-2x"></i>
+          </button>
         </footer>
       </div>
     </li>
