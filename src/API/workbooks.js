c333bc75bf0fe105abf4aec89d49a7d43718b669
ssunpa <bestpa.1130@gmail.com>
2022-01-26 18:21:14 +0900

    Fix : 만료토큰 재설정

--- src/API/workbooks.js
+++ src/API/workbooks.js
@@ -6,7 +6,7 @@ class WorkBooks {
             baseURL: 'http://54.180.103.35:3000/api/v1',
             headers: {
                 Authorization:
-                    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1va2hzMDBAbmF2ZXIuY29tIiwiaWF0IjoxNjQyNTAwODY0LCJleHAiOjE2NDMxMDU2NjR9.opVYGMcjDA18JNqDpdXHQfSJ7PCklVCLKDrtc0WzVBU',
+                    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1va2hzMDBAbmF2ZXIuY29tIiwiaWF0IjoxNjQzMTIyNzg5LCJleHAiOjE2NDM3Mjc1ODl9.s6Hu-TP12Ewo3eSEpQ0Hrd0tD-wB8R9AZmb--kVTtX8',
             },
         });
     }
