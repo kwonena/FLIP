a494562224fa1eaa35dc55c5abc92515321abe92
KwonEna <eeena214@gmail.com>
2022-01-28 01:32:46 +0900

    Style : addCard component UI 수정

--- src/components/addCard/addCard.jsx
+++ src/components/addCard/addCard.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import styles from "./addCard.module.css";
 
-const AddCard = ({ book, open, close, createOrUpdateCard }) => {
+const AddCard = ({ book, title, open, close, createOrUpdateCard }) => {
   const questionRef = useRef();
   const resultRef = useRef();
 
@@ -14,12 +14,12 @@ const AddCard = ({ book, open, close, createOrUpdateCard }) => {
     <section className={open ? `styles.show` : `styles.hidden`}>
       {open && (
         <section className={styles.card}>
-          <section className={styles.titleBox}>
-            <span className={styles.name}>문제집 이름</span>
+          <div className={styles.titleBox}>
+            <span className={styles.name}>{title}</span>
             <button className={styles.closeBtn} onClick={close}>
               <i class="fas fa-times fa-2x"></i>
             </button>
-          </section>
+          </div>
           <section className={styles.textBox}>
             <span className={styles.text}>문제</span>
             <input type="text" className={styles.textarea} ref={questionRef} />
