b2a49a30c280aabc6aad85f9ccf8e9603276a7a7
ssunpa <bestpa.1130@gmail.com>
2022-01-29 23:02:53 +0900

    Fix : 전체 문제집 조회를 내 문제집 조회로 수정

--- src/API/workbooks.js
+++ src/API/workbooks.js
@@ -1,30 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 class WorkBooks {
-    constructor() {
-        this.workBooks = axios.create({
-            baseURL: 'http://54.180.103.35:3000/api/v1',
-            headers: {
-                Authorization:
-                    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1va2hzMDBAbmF2ZXIuY29tIiwiaWF0IjoxNjQzMTIyNzg5LCJleHAiOjE2NDM3Mjc1ODl9.s6Hu-TP12Ewo3eSEpQ0Hrd0tD-wB8R9AZmb--kVTtX8',
-            },
-        });
-    }
+  constructor() {
+    this.workBooks = axios.create({
+      baseURL: "http://54.180.103.35:3000/api/v1",
+      headers: {
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1va2hzMDBAbmF2ZXIuY29tIiwiaWF0IjoxNjQzMTIyNzg5LCJleHAiOjE2NDM3Mjc1ODl9.s6Hu-TP12Ewo3eSEpQ0Hrd0tD-wB8R9AZmb--kVTtX8",
+      },
+    });
+  }
 
-    async showBooks(page) {
-        const response = await this.workBooks.get('workbooks', {
-            params: {
-                page: page,
-                limit: 4,
-            },
-        });
-        return response.data.items;
-    }
+  async showBooks(page) {
+    const response = await this.workBooks.get("workbooks/me", {
+      params: {
+        page: page,
+        limit: 4,
+      },
+    });
+    return response.data.items;
+  }
 
-    async deleteBooks(id) {
-        const response = await this.workBooks.delete(`workbooks/${id}`);
-        return response.status;
-    }
+  async deleteBooks(id) {
+    const response = await this.workBooks.delete(`workbooks/${id}`);
+    return response.status;
+  }
 }
 
 export default WorkBooks;
