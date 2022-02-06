f99a301bb4613f6fa1a00117e07b0edd9fe053d7
KwonEna <eeena214@gmail.com>
2022-02-06 23:06:22 +0900

    Style : quizCard component

--- src/components/quizCard/quizCard.jsx
+++ src/components/quizCard/quizCard.jsx
@@ -1,60 +1,62 @@
-import React, { useState } from 'react';
-import ReactCardFlip from 'react-card-flip';
-import styles from './quizCard.module.css';
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import styles from "./quizCard.module.css";
 
 const QuizCard = ({ quiz }) => {
-    const [page, setPage] = useState(0);
-    const [card, setCard] = useState(quiz[page].cards); // 보여지는 카드
+  const [page, setPage] = useState(0);
+  const [card, setCard] = useState(quiz[page].cards); // 보여지는 카드
 
-    // 카드 페이징
-    const handleNext = () => {
-        setIsFlipped(false);
-        const next = page + 1;
-        if (next < quiz.length) {
-            setCard(quiz[next].cards);
-            setPage(next);
-        } else {
-            alert('마지막 카드 입니다!');
-        }
-    };
+  // 카드 페이징
+  const handleNext = () => {
+    setIsFlipped(false);
+    const next = page + 1;
+    if (next < quiz.length) {
+      setCard(quiz[next].cards);
+      setPage(next);
+    } else {
+      alert("마지막 카드 입니다!");
+    }
+  };
 
-    const handlePrev = () => {
-        setIsFlipped(false);
-        const prev = page - 1;
-        if (prev < 0) {
-            alert('첫번째 카드 입니다!');
-        } else {
-            setCard(quiz[prev].cards);
-            setPage(prev);
-        }
-    };
+  const handlePrev = () => {
+    setIsFlipped(false);
+    const prev = page - 1;
+    if (prev < 0) {
+      alert("첫번째 카드 입니다!");
+    } else {
+      setCard(quiz[prev].cards);
+      setPage(prev);
+    }
+  };
 
-    const [isFlipped, setIsFlipped] = useState(false);
-    const handleClick = () => {
-        setIsFlipped(!isFlipped);
-    };
-    return (
-        <section className={styles.quiz}>
-            <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-                <button className={styles.cardFront} onClick={handleClick}>
-                    <div className={styles.cardName}>{quiz[page].title}</div>
-                    <div className={styles.cardContent}>{card.question}</div>
-                </button>
-                <button className={styles.cardBack} onClick={handleClick}>
-                    {card.result}
-                </button>
-            </ReactCardFlip>
-            <span className={styles.cardPage}>
-                <button onClick={handlePrev}>
-                    <i className="fas fa-arrow-left"></i>
-                </button>
-                {page + 1} / {quiz.length}
-                <button onClick={handleNext}>
-                    <i className="fas fa-arrow-right"></i>
-                </button>
-            </span>
-        </section>
-    );
+  const [isFlipped, setIsFlipped] = useState(false);
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+  return (
+    <section className={styles.quiz}>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <button className={styles.cardFront} onClick={handleClick}>
+          <div className={styles.cardName}>
+            문제집 제목 : {quiz[page].title}
+          </div>
+          <div className={styles.cardContent}>{card.question}</div>
+        </button>
+        <button className={styles.cardBack} onClick={handleClick}>
+          {card.result}
+        </button>
+      </ReactCardFlip>
+      <span className={styles.cardPage}>
+        <button onClick={handlePrev}>
+          <i className="fas fa-arrow-left"></i>
+        </button>
+        {page + 1} / {quiz.length}
+        <button onClick={handleNext}>
+          <i className="fas fa-arrow-right"></i>
+        </button>
+      </span>
+    </section>
+  );
 };
 
 export default QuizCard;
